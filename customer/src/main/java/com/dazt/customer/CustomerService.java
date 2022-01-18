cf1ed5faa4bee8c694aff5dfbe8394a5ed2879f6
Diego <diego.zavala.t@gmail.com>
2022-01-18 15:47:54 -0500

    Implementing Service Discovery - Eureka Server module

--- customer/src/main/java/com/dazt/customer/CustomerService.java
+++ customer/src/main/java/com/dazt/customer/CustomerService.java
@@ -23,7 +23,7 @@ public class CustomerService {
         //TODO check if email is not taken
         //TODO check if fraudster
         FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                "http://FRAUD/api/v1/fraud-check/{customerId}",
                 FraudCheckResponse.class,
                 customer.getId());
         if(fraudCheckResponse != null && fraudCheckResponse.isFraudster()) {
